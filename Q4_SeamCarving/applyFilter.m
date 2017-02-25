584c30f0422c6ae5394b39a838fd3072ec022898
Ian Wood <ian.wood108@gmail.com>
2017-02-25 13:47:05 -0500

    Problem 1

--- Q4_SeamCarving/applyFilter.m
+++ Q4_SeamCarving/applyFilter.m
@@ -2,12 +2,13 @@ function outIm = applyFilter(im, F)
 % take grayscale image F and apply the filter F (with odd dimensions) to F
 
 [r, c] = size(im);
-padded = vertcat(zeros(1, r + 2), horzcat(zeros(c, 1), im, zeros(c, 1)), zeros(1, r + 2));
+[r_F, c_F] = size(F);
+padded = horzcat(zeros(r + 2, 1), vertcat(zeros(1, c), im, zeros(1, c)), zeros(r + 2, 1));
 outIm = zeros(r, c);
 
 for i = 1:r
     for j = 1:c
-        x = padded(i:i + 2, j:j + 2) .* F;        
+        x = padded(i:i + (r_F - 1), j:j + (c_F - 1)) .* F;        
         outIm(i, j) = sum(x(:));
     end
 end
