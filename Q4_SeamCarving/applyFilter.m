ab5e4745b43b524408013fb3e83fa8fe2e06bc02
Ian Wood <ian.wood108@gmail.com>
2017-02-27 00:24:04 -0500

    Problem 2, and problem 1 fix

--- Q4_SeamCarving/applyFilter.m
+++ Q4_SeamCarving/applyFilter.m
@@ -2,12 +2,18 @@ function outIm = applyFilter(im, F)
 % take grayscale image F and apply the filter F (with odd dimensions) to F
 
 [r, c] = size(im);
-[r_F, c_F] = size(F);
-padded = horzcat(zeros(r + 2, 1), vertcat(zeros(1, c), im, zeros(1, c)), zeros(r + 2, 1));
 outIm = zeros(r, c);
 
+[r_F, c_F] = size(F);
+rOffset = (r_F - 1) / 2;
+cOffset = (c_F - 1) / 2;
+
+vertPadded = vertcat(zeros(rOffset, c), im, zeros(rOffset, c));
+horzPadding = zeros(r + (rOffset * 2), cOffset);
+padded = horzcat(horzPadding, vertPadded, horzPadding);
+
 for i = 1:r
-    for j = 1:c
+    for j = 1:c        
         x = padded(i:i + (r_F - 1), j:j + (c_F - 1)) .* F;        
         outIm(i, j) = sum(x(:));
     end
--- Q4_SeamCarving/computeEngGrad.m
+++ Q4_SeamCarving/computeEngGrad.m
@@ -1,2 +1,4 @@
-
-function eng = computeEngGrad(im,F)
+function eng = computeEngGrad(im, F)
+gray = (im(:, :, 1) + im(:, :, 2) + im(:, :, 3)) / 3;
+eng = sqrt(applyFilter(gray, F) .^ 2 + applyFilter(gray, transpose(F)) .^ 2); 
+end
